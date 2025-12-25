49c222ae99301b655798e56126bc3c1937414b24
barbarastefany <barbara.stefany@outlook.com.br>
2025-12-25 20:48:37 -0300

    Innitial commit

---
+++ src/test/java/com/barbara/locadorajogos/LocadoraJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbara.locadorajogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraJogosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
